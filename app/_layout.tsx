da440f60209569bd7b9581b1c08b3601f111bb18
Joël Kuijper <joel@deindruk.nl>
2023-07-14 21:34:27 +0200

    Initial commit
    Generated by create-expo-app 2.0.3.

---
+++ app/_layout.tsx
@@ -0,0 +1,47 @@
+import { Suspense } from "react";
+import { useColorScheme } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { TamaguiProvider, Text, Theme } from "tamagui";
+
+import { MySafeAreaView } from "../components/MySafeAreaView";
+import config from "../tamagui.config";
+
+export default function Layout() {
+  const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf")
+  });
+
+  if (!loaded) {
+    return <SplashScreen />;
+  }
+
+  return (
+    <TamaguiProvider config={config}>
+      <Suspense fallback={<Text>Loading...</Text>}>
+        <Theme name={colorScheme}>
+          <ThemeProvider
+            value={colorScheme === "light" ? DefaultTheme : DarkTheme}
+          >
+            <MySafeAreaView>
+              <Stack
+                screenOptions={{
+                  headerShown: false
+                }}
+              />
+            </MySafeAreaView>
+          </ThemeProvider>
+        </Theme>
+      </Suspense>
+    </TamaguiProvider>
+  );
+}
